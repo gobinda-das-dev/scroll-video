a6fda5788cf73ba6c7d1ee96ca446e741d651b9a
Gobinda Das <gobinda993720@gmail.com>
2024-05-14 10:59:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const $ = (e, p = document) => document.querySelector(e, p);
+const $$ = (e, p = document) => document.querySelectorAll(e, p);
+
+const img = $('img');
+
+
+window.onload = () => {
+
+  const lenis = new Lenis({
+    wheelMultiplier: 0.6,
+    syncTouch: true,
+    touchMultiplier: 0.6
+  })
+
+  lenis.on('scroll', ScrollTrigger.update)
+  gsap.ticker.add(time => lenis.raf(time * 2000))
+  gsap.ticker.lagSmoothing(0)
+
+
+  ScrollTrigger.create({
+    ease: 'none',
+    scroller: document.body,
+    trigger: img,
+    scrub: true,
+    markers: true,
+    start: 'top top',
+    pin: img,
+    end: 'bottom -200%',
+    onUpdate: p => {
+      const mappedPrg = gsap.utils.mapRange(0, 1, 1, 151, p.progress);
+      const mappedRoundedPrg = Math.round(mappedPrg);
+      const formattedFrameNumber = String(mappedRoundedPrg).padStart(3, '0');
+      img.src = `./images/frame-${formattedFrameNumber}.jpg`;
+    }
+  })
+}
